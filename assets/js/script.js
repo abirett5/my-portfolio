02623923a068ecec9c2d8c21b1677fb741a22a5e
Ashfaque Hossain Abir <108783774+abirett5@users.noreply.github.com>
2023-09-01 19:51:02 +0600

    Updated

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,24 @@
  */
 
 "use strict";
+
+/**
+ * Light & Dark Mode
+ */
+
+const $themeBtn = document.querySelector("[data-theme-btn]"); // {NodeElement}
+const $HTML = document.documentElement; // {NodeElement}
+let isDark = window.matchMedia("[prefers-color-scheme]").matches; // {Boolean | String}
+
+if(sessionStorage.getItem("theme")) {
+    $HTML.dataset.theme = sessionStorage.getItem("theme");
+}else {
+    $HTML.dataset.theme = isDark ? "dark" : "light";
+};
+
+const changeTheme = () => {
+    $HTML.dataset.theme = sessionStorage.getItem("theme") === "light" ? "dark" : "light";
+    sessionStorage.getItem("theme", $HTML.dataset.theme);
+};
+
+$themeBtn.addEventListener("click", changeTheme);
